e92c9bea5e91d8271ca14d5ff48a1ec10465b917
visuapp4u <42931597+visuapp4u@users.noreply.github.com>
2018-09-19 17:56:10 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,14 @@
-class hello {
- public static void main (string args[]){
-     system.out.printin("Hello All for the second time");
-   }
-}
+public class hello {
 
+    public static void main(String[] args) {
+
+        int num = 10;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
